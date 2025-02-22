164163f6217661e45b8c7c7b7ad0fc1506f3d578
spark <innocentchris@gmail.com>
2025-02-22 17:39:53 +0900

    리턴 오류 수정

--- trc/lambda/download/get.ts
+++ trc/lambda/download/get.ts
@@ -34,49 +34,59 @@ export const apiSpec = {
 
 export async function lambdaHandler(event: APIGatewayProxyEvent & { v3TestProfile: AwsCredentialIdentityProvider }) {
 	console.log(JSON.stringify(event));
-	const { code } = event.queryStringParameters as any;
+
+	try {
+
+		const { code } = event.queryStringParameters as any;
+		const dynamoDBClient = new DynamoDBClient({
+			region: "ap-northeast-2",
+			credentials: event.v3TestProfile,
+		});
+		const docClient = DynamoDBDocumentClient.from(dynamoDBClient);
+
+		let data = await ddbUtil.query(docClient, `${process.env.service}-${process.env.stage}-data`, ["code"], [code], { rawTableName: true });
+
+		if (data.Items.length === 0) {
+			return {
+				statusCode: 404,
+				body: JSON.stringify({ message: "Not Found" }),
+			};
+		}
+		const fileData = data.Items[0];
 
 
-	const dynamoDBClient = new DynamoDBClient({
-		region: "ap-northeast-2",
-		credentials: event.v3TestProfile,
-	});
-	const docClient = DynamoDBDocumentClient.from(dynamoDBClient);
 
-	let data = await ddbUtil.query(docClient, `${process.env.service}-${process.env.stage}-data`, ["code"], [code], { rawTableName: true });
+		let bucket = fileData.bucket
+		let key = fileData.key
+		let status = fileData.status
+		const originalFilename = key.split("/").reverse()[0];
 
-	if (data.Items.length === 0) {
+		const command = new GetObjectCommand({
+			Bucket: bucket,
+			Key: key,
+			ResponseContentDisposition: `attachment; filename =${encodeURI(originalFilename)}`,
+		});
+
+		const url = await getSignedUrl(new S3Client({
+			region: process.env.region!,
+			credentials: event.v3TestProfile,
+		}), command, { expiresIn: 6000 });
+
+		console.log("return url", url);
 		return {
-			statusCode: 404,
-			body: JSON.stringify({ message: "Not Found" }),
+			statusCode: 200,
+			body: JSON.stringify({
+				url: url
+			}),
 		};
 	}
-	const fileData = data.Items[0];
+	catch (e) {
+		console.log("error:", e);
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ message: "Internal Server Error" }),
+		};
+	}
 
-
-
-	let bucket = fileData.bucket
-	let key = fileData.key
-	let status = fileData.status
-	const originalFilename = key.split("/").reverse()[0];
-
-	const command = new GetObjectCommand({
-		Bucket: bucket,
-		Key: key,
-		ResponseContentDisposition: `attachment; filename =${encodeURI(originalFilename)}`,
-	});
-
-	const url = await getSignedUrl(new S3Client({
-		region: process.env.region!,
-		credentials: event.v3TestProfile,
-	}), command, { expiresIn: 6000 });
-
-	return {
-		statusCode: 200,
-		body: {
-			url: url
-		},
-	};
 }
-
 export const handler = middy().handler(lambdaHandler);
